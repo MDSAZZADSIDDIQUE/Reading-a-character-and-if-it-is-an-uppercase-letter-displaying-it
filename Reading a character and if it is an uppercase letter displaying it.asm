a530d3ff96a0ff7774cf7cca5f5b7920c17ed33e
MDSAZZADSIDDIQUE <mdsazzadsiddique0@gmail.com>
2023-03-23 21:36:23 +0600

    Initial commit

---
+++ Reading a character and if it is an uppercase letter displaying it.asm
@@ -0,0 +1,27 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+.CODE
+MAIN PROC FAR
+    MOV AX, @DATA
+    MOV DS, AX  
+    MOV AH, 1
+    INT 21H
+    MOV BL, AL
+    CMP BL, 'A'
+    JL END_IF
+    CMP BL, 'Z'
+    JG END_IF
+    MOV AH, 2
+    MOV DL, 10
+    INT 21H
+    MOV DL, 13
+    INT 21H
+    MOV AH, 2
+    MOV DL, BL
+    INT 21H  
+    END_IF:
+    MOV AH, 4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
